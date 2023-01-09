50619bc9031f3a0c3308f13b413557d4f596a237
CooledxD <scribe_85@mail.ru>
2023-01-09 15:40:57 +0700

    init controllers

---
+++ controllers/auth.js
@@ -0,0 +1,29 @@
+// Register
+
+export const register = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    
+  }
+}
+
+// Login
+
+export const login = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    
+  }
+}
+
+// Get me
+
+export const getMe = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    
+  }
+}
