59d43ea1cc34357d61c130f2dce50e248661f89c
CooledxD <scribe_85@mail.ru>
2023-02-02 06:38:07 +0700

    minor

--- middleware/multer.js
+++ middleware/multer.js
@@ -4,7 +4,6 @@ import path from 'path'
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    console.log(file)
     cb(null, 'uploads')
   },
   filename: (req, file, cb) => {
