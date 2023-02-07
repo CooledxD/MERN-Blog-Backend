570809a8995b38ecb0d1073a1fe24683f521dc68
CooledxD <scribe_85@mail.ru>
2023-02-07 16:54:11 +0700

    controllers: userControllers refactoring

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -3,7 +3,6 @@ import { dirname } from 'node:path/win32'
 import { fileURLToPath } from 'url';
 
 import User from '../models/userModel.js'
-import Post from '../models/postModel.js'
 
 // Back __dirname
 const __dirname = dirname(fileURLToPath(import.meta.url))
@@ -40,17 +39,11 @@ export const getUser = async(req, res) => {
     const { userId } = req
     const user = await User.findById(userId)
 
-    const posts = await Promise.all(
-      user.posts.map(post => {
-        return Post.findById(post._id)
-      })
-    )
-
     res.status(200).json({
       user: {
         username: user.username,
         avatar: user.avatar,
-        posts
+        posts: user.posts
       }
     })
   } catch (error) {
