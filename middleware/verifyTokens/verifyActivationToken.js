761549a1233572a69be9ac8a85473c67a3a5f8bc
CooledxD <scribe_85@mail.ru>
2023-02-19 15:27:37 +0700

    middleware: the verification of the account activation token has been moved to the middleware

---
+++ middleware/verifyTokens/verifyActivationToken.js
@@ -0,0 +1,40 @@
+import Joi from 'joi'
+import jwt from 'jsonwebtoken'
+
+// Joi schema
+const activationTokenSchema = Joi.object({
+  activationToken: Joi.string()
+    .required()
+})
+
+export const verifyActivationToken = async (req, res, next) => {
+  try {
+    // validation of the request body
+    const { error } = await activationTokenSchema.validateAsync(req.body, {
+      errors: {
+        wrap: {
+          label: false
+        }
+      }
+    })
+
+    if (error) {
+      throw new Error(error.message)
+    }
+
+    // token validation
+    const newUserData = jwt.verify(
+      req.body.activationToken, 
+      process.env.JWT_ACTIVATION_SECRET
+    )
+
+    // we pass the data from the token to the request body
+    req.body = newUserData
+
+    next()
+  } catch (error) {
+    res.status(400).json({
+      message: error.message
+    })
+  }
+}
