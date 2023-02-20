1a1db8f955ff72a39bc29a6049ed31f1d6ab8175
CooledxD <scribe_85@mail.ru>
2023-02-20 17:51:47 +0700

    middleware: added data validation for get a post

---
+++ middleware/validation/validationGetPost.js
@@ -0,0 +1,30 @@
+import Joi from "joi";
+
+import Post from "../../models/postModel";
+
+export const validationGetPost = async (req, res, next) => {
+  try {
+    // validation postId
+    Joi.assert(
+      req.params.postId, 
+      Joi.string()
+        .pattern(/^[a-z0-9]+$/)
+        .required()
+        .messages({
+          'string.pattern.base': 'incorrect post id'
+        })
+    )
+
+    const post = Post.findById(req.params.postId)
+
+    if (!post) {
+      throw new Error('There is no such post')
+    }
+
+    next()
+  } catch (error) {
+    res.status(400).json({
+      message: error.message
+    })
+  }
+}
